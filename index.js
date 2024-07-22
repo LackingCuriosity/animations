40420cacdbea61a65a6b667a455cc592505789dc
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2024-07-22 17:23:06 -0400

    animations page done

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", e => {
     document.addEventListener("scroll", e=> {
         // Opening Page
         document.querySelector('.scrollTip').style.opacity = "0"
-        document.querySelector(".openingPage").style.opacity = Math.max(0, 1 - (window.scrollY/window.innerHeight) * 1.6)
+        document.querySelector(".openingPage").style.opacity = Math.max(0, 1 - (window.scrollY/window.innerHeight) * .9)
 
         //Animation Page Transition (STARTS AT 100vh SHIFTED TO 50vh)
         let animationsPage = document.querySelector('.animationsPage')
-        animationsPage.style.marginTop = 100 - window.scrollY/window.innerHeight * 50 + 'vh'
-        document.querySelector('.slideshowTop').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)/100 * 350 * 5 * 2}px`
-        document.querySelector('.slideshowMid').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)/100 * 350 * 5 * 2}px`
-        document.querySelector('.slideshowBot').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)/100 * 350 * 5 * 2}px`
+        animationsPage.style.marginTop = 100 - window.scrollY/window.innerHeight * 90 + 'vh'
     })
+
+    // Copy and setup onclick link
     document.querySelectorAll('.slideshowItemSet').forEach(element => {
         let parent = element.parentElement
         for(let child of element.childNodes) {
+            // Copy for carosuel effect
             if (child.nodeType === "text" || child.innerHTML === undefined) {
                 continue
             }
@@ -25,6 +25,15 @@ document.addEventListener("DOMContentLoaded", e => {
                     childDiv.appendChild(newChild)
                 }
             }
+            // add links
+            console.log(child.dataset.link)
+            if (child.dataset.link !== undefined) {
+                child.onclick = () => window.location.href = child.dataset.link
+                newChild.onclick = () => window.location.href =  child.dataset.link
+            }
+
+
+
         }
 
     })
