a01ea1310e947e8f6f2d7740abdd7c1ac4561707
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2024-07-22 10:27:31 -0400

    sliding animation work

--- index.js
+++ index.js
@@ -7,10 +7,26 @@ document.addEventListener("DOMContentLoaded", e => {
         //Animation Page Transition (STARTS AT 100vh SHIFTED TO 50vh)
         let animationsPage = document.querySelector('.animationsPage')
         animationsPage.style.marginTop = 100 - window.scrollY/window.innerHeight * 50 + 'vh'
-        document.querySelector('.slideshowTop').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)}vw`
-        document.querySelector('.slideshowMid').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)}vw`
-        document.querySelector('.slideshowBot').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)}vw`
+        document.querySelector('.slideshowTop').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)/100 * 350 * 5 * 2}px`
+        document.querySelector('.slideshowMid').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)/100 * 350 * 5 * 2}px`
+        document.querySelector('.slideshowBot').style.width = `${Math.min(100, (window.scrollY/window.innerHeight) * 50)/100 * 350 * 5 * 2}px`
     })
-    document.querySelector('.slideshowTop')
+    document.querySelectorAll('.slideshowItemSet').forEach(element => {
+        let parent = element.parentElement
+        for(let child of element.childNodes) {
+            if (child.nodeType === "text" || child.innerHTML === undefined) {
+                continue
+            }
+            let newChild = document.createElement("div")
+            newChild.innerHTML = child.innerHTML
+            newChild.className = "slideshowItem"
+            for (let childDiv of parent.childNodes) {
+                if(childDiv.className === "slideshowItemSetCopy") {
+                    childDiv.appendChild(newChild)
+                }
+            }
+        }
+
+    })
 
 })
